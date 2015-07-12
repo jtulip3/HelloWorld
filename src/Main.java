aac2ee01189fa3a7027c17cb0dbf3b672cf9ec96
Jim Jimmac <jtulip58@yahoo.com.au>
2015-07-12 20:37:57 +1000

    Modded HelloWorld line and added Hello3

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world from master");
+		System.out.println("Hello world from master and jtulip3");
 		System.out.println("Hello line 1");
+		System.out.println("Hello line 3");
 		
 
 	}
